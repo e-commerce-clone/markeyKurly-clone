4b50036a567ec81d57a4852e1c3a0393c838fb38
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-01-12 11:06:38 +0900

    fade in 추가

--- 2021-01-14/js/good_list01.js
+++ 2021-01-14/js/good_list01.js
@@ -9,40 +9,43 @@ window.addEventListener('DOMContentLoaded',function(){
         var noBody = $('.noBody');
 
 
+        var product_name = [];
+        var product_price = [];
+        var product_cost = [];
 
 
-
-
-
-        var product_name1 = $(' li:nth-child(1) .name').text();
-        var product_name2 = $(' li:nth-child(2) .name').text();
-
-        var product_price1 = $(' li:nth-child(1) .price').text();
-        var product_price2 = $(' li:nth-child(2) .price').text();
-
-        var product_cost1 = $('#product_cost1').val();
-        var product_cost2 = $('#product_cost2').val();
-        console.log(product_cost1); // 확인차
-
-        $('button[name=chk]:eq(0)').click(function(){
-
-            $('#cartPut').css('display','block');
-            $('.bg_loading').css('display','block');
-            $('#cartPut .name').text(product_name1);
-            $('#cartPut .dc_price').text(product_price1);
-            $('#cartPut .num').text(product_price1);
+        for(i=0; i < 13; i++){
+            product_name[i] = $('li:nth-child('+ i +') .name').text();
             
+            product_price[i] = $('li:nth-child('+ i +') .price').text();
+            
+            product_cost[i] = $(' li:nth-child('+ i +') input').val();
+            
+        }
 
-           
+        
+
+        
+        
+
+        
+        function cart_button(i){                                // 장바구니 버튼 클릭했을떄 수량,적립금 설정함수
+            // $('#cartPut').css('display','block');
+            $('#cartPut').fadeIn(300);                 // 서서히 보이게 설정 완료
+            $('.bg_loading').css('display','block');
+            $('#cartPut .name').text(product_name[i]);
+            $('#cartPut .dc_price').text(product_price[i]);
+            $('#cartPut .num').text(product_price[i]);
+        
             noBody.addClass('noBody_on');
-
-
-
+        
+        
+        
             var number = $('.inp').val();   //수량
-            var cost = product_cost1;   // 상품의 가격을 설정해줘야됨.
+            var cost = product_cost[i];   // 상품의 가격을 설정해줘야됨.
             $('.emph').text((number*cost)/20 + '원 적립');
-
-
+        
+        
             $('.down').click(function(){
                 if(number > 0){
                     number--;
@@ -50,6 +53,12 @@ window.addEventListener('DOMContentLoaded',function(){
                     if(number == 0){               // 0원이면 0출력
                         $(".num").text(0);
                     }
+
+                    else if(number*cost < 1000){
+                        $(".num").text(number*cost);
+                    }
+
+
                     else if((number*cost % 1000) == 0){
                         
                         $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
@@ -59,21 +68,21 @@ window.addEventListener('DOMContentLoaded',function(){
                     $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
                     }
                 }
-
+        
                 if(number < 0){             //0 이하면 false 반환
                     return false;
                 }
-
-
+        
+        
                 $('.emph').text((number*cost)/20 + '원 적립');
                 
             
                 
                 
             });
-
-
-
+        
+        
+        
             $('.up').click(function(){
                 number++;
                 $(".inp").val(number);
@@ -81,126 +90,86 @@ window.addEventListener('DOMContentLoaded',function(){
                     $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
                 }
 
+                else if(number*cost < 1000){
+                    $(".num").text(number*cost);
+                }
+        
                 else{
                     $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
                     
-
+        
                 }
-
-
-
+        
+        
+        
                 
                 $('.emph').text((number*cost)/20 + '원 적립');
                 
-
+        
                 
             });
-
+        
             $('.btn_type2').click(function(){
                 $('#cartPut').css('display','none');
                 $('.bg_loading').css('display','none');
-    
+        
                 noBody.removeClass('noBody_on');
                 ///// 닫기 눌렀으면 원래값으로 초기화 시켜줘야됨 ////
                 number = 1;
                 $('.inp').val(1); 
                 $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
                 $('.emph').text((number*cost)/20 + '원 적립');
-    
+        
                 
             });
-                
-                
+        
+        }
+        
+        
+        
+        $('button[name=chk]:eq(0)').click(function(){              //굉장히 줄이고싶다... 어떻게 줄이지 ... 하..
+            cart_button(1);
         });
-
         $('button[name=chk]:eq(1)').click(function(){
-
-            $('#cartPut').css('display','block');
-            $('.bg_loading').css('display','block');
-            $('#cartPut .name').text(product_name2);
-            $('#cartPut .dc_price').text(product_price2);
-            $('#cartPut .num').text(product_price2);
-            
-            
-
-           
-            noBody.addClass('noBody_on');
-
-            var number = $('.inp').val();   //수량
-            var cost = product_cost2;   // 상품의 가격을 설정해줘야됨.
-            $('.emph').text((number*cost)/20 + '원 적립');
-
-
-            $('.down').click(function(){
-                if(number > 0){
-                    number--;
-                    $(".inp").val(number);
-                    if(number == 0){               // 0원이면 0출력
-                        $(".num").text(0);
-                    }
-                    else if((number*cost % 1000) == 0){
-                        
-                        $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
-                    }
-                    else{
-                    
-                    $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
-                    }
-                }
-
-                if(number < 0){             //0 이하면 false 반환
-                    return false;
-                }
-
-
-                $('.emph').text((number*cost)/20 + '원 적립');
-                
-            
-                
-                
-            });
-
-
-
-            $('.up').click(function(){
-                number++;
-                $(".inp").val(number);
-                if((number*cost % 1000) == 0){
-                    $(".num").text(Math.floor(number*cost/1000) + ',' + '000');
-                }
-
-                else{
-                    $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
-                    
-
-                }
-
-
-
-                
-                $('.emph').text((number*cost)/20 + '원 적립');
-                
-
-                
-            });
-
-
-            $('.btn_type2').click(function(){
-                $('#cartPut').css('display','none');
-                $('.bg_loading').css('display','none');
-    
-                noBody.removeClass('noBody_on');
-                ///// 닫기 눌렀으면 원래값으로 초기화 시켜줘야됨 ////
-                number = 1;
-                $('.inp').val(1); 
-                $(".num").text(Math.floor(number*cost/1000) + ',' + number*cost%1000);
-                $('.emph').text((number*cost)/20 + '원 적립');
-    
-                
-            });
-            
-            
+            cart_button(2);
         });
+        $('button[name=chk]:eq(2)').click(function(){
+            cart_button(3);
+        });
+        $('button[name=chk]:eq(3)').click(function(){
+            cart_button(4);
+        });
+        $('button[name=chk]:eq(4)').click(function(){
+            cart_button(5);
+        });
+        $('button[name=chk]:eq(5)').click(function(){
+            cart_button(6);
+        });
+        $('button[name=chk]:eq(6)').click(function(){
+            cart_button(7);
+        });
+        $('button[name=chk]:eq(7)').click(function(){
+            cart_button(8);
+        });
+        $('button[name=chk]:eq(8)').click(function(){
+            cart_button(9);
+        });
+        $('button[name=chk]:eq(9)').click(function(){
+            cart_button(10);
+        });
+        $('button[name=chk]:eq(10)').click(function(){
+            cart_button(11);
+        });
+        $('button[name=chk]:eq(11)').click(function(){
+            cart_button(12);
+        });
+
+
+
+
+
+
+        
 
 
 
@@ -219,37 +188,6 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
         
-
-
-
-
-        
-        
-
-
-
-
-
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
